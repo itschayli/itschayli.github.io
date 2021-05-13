3b17647e793ec34e7875b3a70989e2a945a9b732
itschayli <83977996+itschayli@users.noreply.github.com>
2021-05-13 12:13:45 -0500

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -46,7 +46,7 @@ export var soundArray = []; //list of sounds loaded
 export var wallSound = new soundFile("tap-resonant.mp3"); //load sound
 soundArray.push(wallSound); //add sound to list of sounds
 
-export var paddleSound = new soundFile("tap-percussive.mp3");
+export var paddleSound = new soundFile("mypaddlenoise.mp3");
 soundArray.push(paddleSound);
 
 export var scoreSound = new soundFile("phaserDown2.mp3");
