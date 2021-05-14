c810761866c833edcf898c59f129f2dcd2e2ffbf
itschayli <83977996+itschayli@users.noreply.github.com>
2021-05-14 14:14:00 -0500

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -43,13 +43,13 @@ export var soundArray = []; //list of sounds loaded
 
 //Here is where all the Sound File Players Start
 
-export var wallSound = new soundFile("echono.mp3"); //load sound
+export var wallSound = new soundFile("score.mp3"); //load sound
 soundArray.push(wallSound); //add sound to list of sounds
 
 export var paddleSound = new soundFile("popnoise.mp3");
 soundArray.push(paddleSound);
 
-export var scoreSound = new soundFile("score.mp3");
+export var scoreSound = new soundFile("echono.mp3");
 soundArray.push(scoreSound);
 
 export var ambientSound = new soundFile("horsehooves.mp3");
