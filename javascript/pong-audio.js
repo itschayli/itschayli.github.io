fe826b56ffc61370f8c7c13076a7b716934d78a7
itschayli <83977996+itschayli@users.noreply.github.com>
2021-05-14 13:55:22 -0500

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -52,7 +52,7 @@ soundArray.push(paddleSound);
 export var scoreSound = new soundFile("score.mp3");
 soundArray.push(scoreSound);
 
-export var ambientSound = new soundFile("awtylong.mp3");
+export var ambientSound = new soundFile("horsehooves.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
 ambientSound.player.volume.value = -10; //turn down volume
